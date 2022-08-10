c7166f4f70de5a86c1789c5124182e10117f407d
vaishak <vaishak7026@gmail.com>
2022-08-10 12:13:06 +0530

    final

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
+from django.shortcuts import HttpResponse
+import requests
 
+
+
+def index(request):
+    r = requests.get('https://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>') 
 def home(request):
 
     if request.method == 'POST':
